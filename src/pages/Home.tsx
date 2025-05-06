9f32b81670ee4d8c47184e2cced403e1e1fa386a
stjernholm <stjernholm@msn.com>
2025-05-06 21:08:56 +0200

    fix: image import

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,12 @@
 import { ReactElement } from "react";
+import imgHeroUrl from "src/assets/img-stina-hero.jpg";
 
 export function Home(): ReactElement {
     return (
         <div className="home">
             <div className="header-space"></div>
             <figure>
-                <img src="src/assets/img-stina-hero.jpg" alt="Stina dancing" />
+                <img src={imgHeroUrl} alt="Stina dancing" />
             </figure>
             <section className="home-text">
                 <h3>Dance Artist</h3>
